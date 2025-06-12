7e5d7d2ea44127193da16bb46724a3850fbe9305
Muhammad Arham Talat Siddiqui <132147584+MakeitMuhammad@users.noreply.github.com>
2025-06-12 21:34:47 +0500

    Update settings.py

--- ardenFYP/settings.py
+++ ardenFYP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,7 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 #manually added media root
-import os
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
